e16f9d021eaebeecd772462c6227a24f30902ba9
jv456njit <jv456@njit.edu>
2024-02-04 02:27:19 -0500

    landing page created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Landing from './landing';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const showLanding = () => {
+  if ((window.location.pathname === "/App.js") || (window.location.pathname === "/")) {
+    return <> <Landing /> </>
+  }
 }
 
-export default App;
+export default () => {
+  return (
+    <div className="ui container">
+      {showLanding()}
+    </div>
+  )
+}
