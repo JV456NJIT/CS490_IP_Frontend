3fe6801dc43c4b6b2285bc7aa9e7d2e9f76d5837
jv456njit <jv456@njit.edu>
2024-02-08 17:19:48 -0500

    Showing customers from DB and search working

--- src/customers.js
+++ src/customers.js
@@ -1,24 +1,120 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './customers.css';
 
-const customers = () =>{
+function Movies(){
+
+    const initialValues = {customerID: null, customerFirstName: null, customerLastName: null}
+    const [customer, setCustomer] = useState([]);
+    const [loaded, setLoaded] = useState(false);
+    const [values, setValues] = useState(initialValues);
+    const [customerPicked, setCustomerPicked] = useState(false);
+    const [customerDetail, setCustomerDetail] = useState([]);
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setValues({
+            ...values,
+            [name]:value,
+        });
+    };
+
+    useEffect(() =>{
+        if(values.customerID=="") values.movieTitle=null;
+        if(values.customerFirstName=="") values.actorFirstName=null;
+        if(values.customerLastName=="") values.actorLastName=null;
+        searchInput(values.customerID, values.customerFirstName, values.customerLastName);
+    },[values]);
+    
+    const searchInput = (customerID, customerFirstName, customerLastName) =>{
+        setLoaded(false);
+        fetch(`http://localhost:8000/customers/${customerID}&${customerFirstName}&${customerLastName}`)
+        .then(response => response.json())
+        .then((customer) => {
+            setCustomer(customer);
+            setLoaded(true);
+        })
+    };
+
+    const customerClick = (customerName) => {
+        setLoaded(false);
+        fetch(`http://localhost:8000/customer/${customerName}`)
+        .then(response => response.json())
+        .then((customerDetail) => {
+            setCustomerDetail(customerDetail);
+            setLoaded(true);
+            setCustomerPicked(true);
+        })
+    };
+
+    const clickClose = () =>{
+        setLoaded(false);
+        setCustomerPicked(false);
+    }
+
     return(
         <>
-            <div class="search-customers">
-                <input type="text" class="search" placeholder="Search for Customers..."></input>
+            <div class="search-container">
+                <input type="text" placeholder="Customer ID" class="search-bar" value={values.customerID} name="customerID"
+                    onChange={handleChange}></input>
+
+                <input type="text" placeholder="Customer First Name" class="search-bar" value={values.customerFirstName} name="customerFirstName"
+                    onChange={handleChange}></input>
+
+                <input type="text" placeholder="Customer Last Name" class="search-bar" value={values.customerLastName} name="customerLastName"
+                    onChange={handleChange}></input>
             </div>
 
-            <div class="customers-container">
-                <div class="grid-child">
-                    <h1>Customer Search Results</h1>
+            {loaded && (
+                <div class="container" onClick={clickClose}>
+                    <table>
+                        <th>Customer ID</th>
+                        <th>Customer First Name</th>
+                        <th>Customer Last Name</th>
+                        {customer.map((customerD, customerI ) => (
+                            <tr key={customerI}
+                            onClick={(e) => customerClick(customerD.customer_id)}>
+                                <td>{customerD.customer_id}</td>
+                                <td>{customerD.first_name}</td>
+                                <td>{customerD.last_name}</td>
+                            </tr>
+                        ))}
+                    </table>
                 </div>
-                
-                <div class="customers-child">
-                    <h1>Customer Details</h1>
+            )}
+            {(loaded && customerPicked) && (
+                <div class="popup-container" onClick={clickClose}>
+                    <div class="popup">
+                        <table>
+                            <th colSpan="100%">Customer Details</th>
+                            {customerDetail.map((customerD, customerI ) => (
+                                <tbody>
+                                    <tr>
+                                        <td>Customer ID:</td>
+                                        <td>{customerD.customer_id}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Customer First Name:</td>
+                                        <td>{customerD.first_name}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Customer Last Name:</td>
+                                        <td>{customerD.last_name}m</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Customer Email:</td>
+                                        <td>{customerD.email}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Customer Creation Date:</td>
+                                        <td>{customerD.create_date}</td>
+                                    </tr>
+                                </tbody>
+                            ))}
+                        </table>
+                    </div>
                 </div>
-            </div>
+            )}
         </>
     );
-};
-
-export default customers;
+}
+export default Movies;
